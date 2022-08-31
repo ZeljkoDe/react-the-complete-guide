f6c10afdb0519e709b1cdd3ea32fd7bfe657a7c9
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-08-31 06:20:02 +0200

    Assignment 2: Time to Practice: Working with Events & State - my solution

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 
 const Expenses = ({ expenses }) => {
+    const [selectedFilter, setSelectedFilter] = useState('')
+
+    const filterHandler = (selected) => {
+        setSelectedFilter(selected);
+    }
+
+    console.log(selectedFilter);
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onFilterSelection={filterHandler} />
             {
                 expenses.map(item => (
                     <ExpenseItem
--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -7,6 +7,7 @@ const NewExpense = ({ onAddExpense }) => {
             ...enterdExpenseData,
             id: Math.random().toString()
         };
+        
         onAddExpense(expenseData)
     };
 
