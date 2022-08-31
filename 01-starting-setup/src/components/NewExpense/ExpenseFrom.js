da0987eb54a098ea523dbb4f27411a351ecca9a4
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-08-31 23:06:29 +0200

    Assignment 4: Time to Practice: Conditional Content - his solution

--- 01-starting-setup/src/components/NewExpense/ExpenseFrom.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseFrom.js
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import './ExpenseFrom.css';
 
-const ExpenseFrom = ({ onSaveExpenseData }) => {
+const ExpenseFrom = ({ onSaveExpenseData, onCancel }) => {
   const [enterdTitle, setEnterdTitle] = useState('');
   const [enterdAmount, setEnterdAmount] = useState('');
   const [enterdDate, setEnterdDate] = useState('');
-  const [addNewButton, setAddNewButton] = useState(false);
-  const [cancleButton, setCancleButton] = useState(false);
 
   const titleChangeHandler = (e) => {
     setEnterdTitle(e.target.value);
@@ -31,57 +29,29 @@ const ExpenseFrom = ({ onSaveExpenseData }) => {
     setEnterdTitle('');
     setEnterdAmount('');
     setEnterdDate('');
-
-    setCancleButton(true);
   };
-
-  const handeAddNewButton = () => {
-    setAddNewButton(true);
-    setCancleButton(false);
-  };
-
-  const handleCancleButton = () => {
-    setCancleButton(true);
-    setAddNewButton(false);
-  };
-
-  if (cancleButton || !addNewButton) {
-    return (
-      <>
-        <div className="new-expense__actions">
-          <button onClick={handeAddNewButton} type='button'>Add New Expense</button>
-        </div>
-      </>
-    );
-  }
 
   return (
-    <>
-      {addNewButton &&
-        <form onSubmit={submitHandler}>
-          <div className="new-expense__controls">
-            <div className="new-expense__control">
-              <label>Title</label>
-              <input type="text" value={enterdTitle} onChange={titleChangeHandler} />
-            </div>
-            <div className="new-expense__control">
-              <label>Amount</label>
-              <input type="number" min="0.01" step="0.01" value={enterdAmount} onChange={amountChangeHandler} />
-            </div>
-            <div className="new-expense__control">
-              <label>Date</label>
-              <input type="date" min="2019-01-01" max="2022-12-31" value={enterdDate} onChange={dateChangeHandler} />
-            </div>
-          </div>
-          <div className="new-expense__actions">
-            <button type='submit'>Add expense</button>
-          </div>
-          <div className="new-expense__actions">
-            <button onClick={handleCancleButton} type='submit'>Cancel</button>
-          </div>
-        </form>
-      }
-    </>
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={enterdTitle} onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" value={enterdAmount} onChange={amountChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" value={enterdDate} onChange={dateChangeHandler} />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type='button' onClick={onCancel}>Cancle</button>
+        <button type='submit'>Add expense</button>
+      </div>
+    </form>
   );
 };
 
--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import ExpenseFrom from './ExpenseFrom';
 import './NewExpense.css';
 
 const NewExpense = ({ onAddExpense }) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enterdExpenseData) => {
         const expenseData = {
             ...enterdExpenseData,
             id: Math.random().toString()
         };
-        
-        onAddExpense(expenseData)
+
+        onAddExpense(expenseData);
+        setIsEditing(false)
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
 
     return (
         <div className='new-expense'>
-            <ExpenseFrom onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseFrom onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
         </div>
+
     );
 };
 
