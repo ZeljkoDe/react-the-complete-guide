dd87b96f2e805f188ce5f3121bbcb93f85e81bac
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-08-31 22:49:25 +0200

    Assignment 4: Time to Practice: Conditional Content - my solution

--- 01-starting-setup/src/components/NewExpense/ExpenseFrom.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseFrom.js
@@ -5,41 +5,17 @@ const ExpenseFrom = ({ onSaveExpenseData }) => {
   const [enterdTitle, setEnterdTitle] = useState('');
   const [enterdAmount, setEnterdAmount] = useState('');
   const [enterdDate, setEnterdDate] = useState('');
-  // const [userInput, setUserInput] = useState({
-  //   enterdTitle: '',
-  //   enterdAmount: '',
-  //   enterdDate: ''
-  // });
+  const [addNewButton, setAddNewButton] = useState(false);
+  const [cancleButton, setCancleButton] = useState(false);
 
   const titleChangeHandler = (e) => {
     setEnterdTitle(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enterdTitle: e.target.value
-    // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enterdTitle: e.target.value };
-    // });
   };
   const amountChangeHandler = (e) => {
     setEnterdAmount(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enterdAmount: e.target.value
-    // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enterdAmount: e.target.value };
-    // });
   };
   const dateChangeHandler = (e) => {
     setEnterdDate(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enterdDate: e.target.value
-    // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enterdDate: e.target.value };
-    // });
   };
   const submitHandler = (e) => {
     e.preventDefault();
@@ -55,28 +31,57 @@ const ExpenseFrom = ({ onSaveExpenseData }) => {
     setEnterdTitle('');
     setEnterdAmount('');
     setEnterdDate('');
+
+    setCancleButton(true);
   };
 
+  const handeAddNewButton = () => {
+    setAddNewButton(true);
+    setCancleButton(false);
+  };
+
+  const handleCancleButton = () => {
+    setCancleButton(true);
+    setAddNewButton(false);
+  };
+
+  if (cancleButton || !addNewButton) {
+    return (
+      <>
+        <div className="new-expense__actions">
+          <button onClick={handeAddNewButton} type='button'>Add New Expense</button>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={enterdTitle} onChange={titleChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={enterdAmount} onChange={amountChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" value={enterdDate} onChange={dateChangeHandler} />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type='submit'>Add expense</button>
-      </div>
-    </form>
+    <>
+      {addNewButton &&
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" value={enterdTitle} onChange={titleChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input type="number" min="0.01" step="0.01" value={enterdAmount} onChange={amountChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input type="date" min="2019-01-01" max="2022-12-31" value={enterdDate} onChange={dateChangeHandler} />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type='submit'>Add expense</button>
+          </div>
+          <div className="new-expense__actions">
+            <button onClick={handleCancleButton} type='submit'>Cancel</button>
+          </div>
+        </form>
+      }
+    </>
   );
 };
 
